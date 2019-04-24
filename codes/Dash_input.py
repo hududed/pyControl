8a77c1402c3ed9c273437d842bb33d6f6ed9ffb4
Hud Wahab <hudwahab@gmail.com>
2019-04-24 17:55:19 -0600

    dash, input

---
+++ codes/Dash_input.py
@@ -0,0 +1,22 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    dcc.Input(id='input', value='Enter something here!', type='text'),
+    html.Div(id='output')
+])
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+)
+def update_value(input_data):
+    return 'Input: "{}"'.format(input_data)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
