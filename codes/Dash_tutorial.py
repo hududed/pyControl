2ad67d4fc23b01f280b6bedc6ebe9da3118f69af
Hud Wahab <hudwahab@gmail.com>
2019-04-24 09:29:22 -0600

    dash bar,line plots

--- codes/Dash_tutorial.py
+++ codes/Dash_tutorial.py
@@ -1,10 +1,24 @@
 import dash
-import dash_core_components as doc
+import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div('Dash Test')
+app.layout = html.Div(children=[
+    html.H1(children='Dash Tutorials'),
+    dcc.Graph(
+        id='example',
+        figure={
+            'data': [
+                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
+                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
+            ],
+            'layout': {
+                'title': 'Basic Dash Example'
+            }
+        }
+    )
+])
 
 if __name__ == '__main__':
     app.run_server()
