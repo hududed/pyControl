c0013a8434938b988339aac6da8e81381a60fc43
SourinDeyUW <55370267+SourinDeyUW@users.noreply.github.com>
2019-12-26 01:09:59 +0600

    Update initial experiment.py

--- Spectrometer Automation/initial experiment.py
+++ Spectrometer Automation/initial experiment.py
@@ -15,23 +15,9 @@ for x in range(20):
     gas.append("Argon")
     pressure.append(np.random.randint(0,100))
 
-row=['Power','Time','Gas','Pressure','Ratio']
+row=['power','time','gas','pressure','ratio']
 with open('dataset-2.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(row)
         writer.writerows(zip(power,time,gas,pressure))
 
-#File 2
-#This will work for setting D to G ratio for 1st 20 random values
-
-import pandas as pd
-def update_ratio(k,value):
-    df = pd.read_csv("dataset-2.csv")
-    df.set_value(k, "Ratio", value)
-    df.to_csv("dataset-2.csv", index=False)
-
-for i in range(22):
-    if i==15:
-        print("Stop Running, Initial Experiment Done")
-        break
-    update_ratio(i,i*i)
