ee3dca0ef3082060de0f39857074decbc6496b43
Hud Wahab <hudwahab@gmail.com>
2023-07-07 16:16:29 -0600

    chore: add more typing

--- GcodeConverter/arcconverter.py
+++ GcodeConverter/arcconverter.py
@@ -8,7 +8,6 @@ class ArcConverter:
     
     def __init__(self, cmd: str, start_xpos: int=0, start_ypos: int=0, step_size: float=0.1, mode: str='abs') -> None:
         
-        self.cmd = cmd
         self.step_size = step_size
         self.mode = mode
         
@@ -23,49 +22,12 @@ class ArcConverter:
         self.output_cmds: list[str] = []
 
         self.parse_cmd(cmd)
-        
-        # #parse cmd
-        # args = self.cmd.split(" ")
-        
-        # if args[0] == 'G02' or args[0] == 'G2': #clockwise arc
-        #     #print('clockwise')
-        #     self.dir: str = 'cw'
-            
-        # elif args[0] == 'G03' or args[0] == 'G3': #counter-clockwise arc
-        #     #print('counter-clockwise')
-        #     self.dir: str = 'ccw'
-        
-        # else:
-        #     #print('invalid arc command provided, defaulting to a clockwise (G2) arc')
-        #     self.dir: str = 'cw'
-            
-        # self.xoffset: float = 0
-        # self.yoffset: float = 0
-        
-        # self.end_xpos: float = 0
-        # self.end_ypos: float = 0
-        # self.center_xpos: float = 0
-        # self.center_ypos: float = 0
-        
-        # for arg in args:
-            
-        #     if arg[0] == 'X':
-        #         self.end_xpos = float(arg[1:])
-                
-        #     elif arg[0] == 'Y':
-        #         self.end_ypos = float(arg[1:])
-                
-        #     elif arg[0] == 'I':
-        #         self.center_xpos = float(arg[1:])
-                
-        #     elif arg[0] == 'J':
-        #         self.center_ypos = float(arg[1:])
 
         #self.radius = self.distance(0, 0, self.center_xpos, self.center_ypos)#OG
         self.radius = self.distance(self.start_xpos, self.start_ypos, self.end_xpos, self.end_ypos)/2
        
         #self.angle = self.arc_angle(self.start_xpos, self.start_ypos, self.end_xpos, self.end_ypos, self.center_xpos, self.center_ypos)#OG
-        self.angle = 180
+        self.angle: float = 180
         #self.angle = self.arc_angle(0, 0, self.end_xpos, self.end_ypos, self.center_xpos, self.center_ypos)
         
         #self.arc_to_lines(self.start_xpos, self.start_ypos, self.end_xpos, self.end_ypos, self.angle)#OG
@@ -77,15 +39,18 @@ class ArcConverter:
 
     def parse_cmd(self, cmd: str) -> None:
         self.cmd = cmd
+        self.parse_arc_command()
+        self.parse_args()
+    
+    def parse_arc_command(self) -> None:
+        arc_commands: dict[str,str] = {'G02': 'cw',
+                        'G2': 'cw',
+                        'G03': 'ccw',
+                        'G3': 'ccw'}
+        self.dir = arc_commands.get(self.cmd.split(" ")[0], 'cw')
+    
+    def parse_args(self) -> None:
         args = self.cmd.split(" ")
-        
-        if args[0] == 'G02' or args[0] == 'G2': #clockwise arc
-            self.dir = 'cw'
-        elif args[0] == 'G03' or args[0] == 'G3': #counter-clockwise arc
-            self.dir = 'ccw'
-        else:
-            self.dir = 'cw'
-            
         for arg in args:
             if arg[0] == 'X':
                 self.end_xpos = float(arg[1:])
@@ -141,7 +106,7 @@ class ArcConverter:
 
 
     def distance_from_arc_point_to_arc_center(self, arc_angle: float, arc_radius: float) -> float:
-        angle = arc_angle/2
+        angle: float = arc_angle/2
         return math.sin(math.radians(angle)) * arc_radius * 2
 
 
@@ -161,26 +126,26 @@ class ArcConverter:
 
     def arc_to_lines(self, x_start: float, y_start: float, x_end: float, y_end: float, angle: float, cw: str) -> None:
         if self.distance(x_start, y_start, x_end, y_end) <= self.step_size:
-            rtrn = 'G1 X{0:0.6f} Y{1:0.6f}'.format(x_end, y_end)
+            rtrn: str = 'G1 X{0:0.6f} Y{1:0.6f}'.format(x_end, y_end)
             self.output_cmds.append(rtrn)
             return
 
-        dist_to_center = self.distance_from_arc_point_to_arc_center(angle/2, self.radius)
-        dist_between_points = self.distance(x_start, y_start, x_end, y_end)
+        dist_to_center: float = self.distance_from_arc_point_to_arc_center(angle/2, self.radius)
+        dist_between_points: float = self.distance(x_start, y_start, x_end, y_end)
 
         # find the x coordinate of the center point
-        pt1_x = (x_start + x_end)/2 if cw else (x_end + x_start)/2
-        pt2_x = (y_start - y_end)/2 if cw else (y_end - y_start)/2
-        pt3 = (2 * dist_to_center)/dist_between_points
-        pt3 = math.pow(pt3, 2)
-        pt3 = pt3 - 1
-        pt3 = math.sqrt(pt3)
-        x = pt1_x + (pt2_x * pt3)
+        pt1_x: float = (x_start + x_end)/2 if cw else (x_end + x_start)/2
+        pt2_x: float = (y_start - y_end)/2 if cw else (y_end - y_start)/2
+        pt3: float = (2 * dist_to_center)/dist_between_points
+        pt3: float = math.pow(pt3, 2)
+        pt3: float = pt3 - 1
+        pt3: float = math.sqrt(pt3)
+        x: float = pt1_x + (pt2_x * pt3)
 
         # find the y coordinate of the center point
-        pt1_y = (y_start + y_end)/2 if cw else (y_end + y_start)/2
-        pt2_y = (x_start - x_end)/2 if cw else (x_end - x_start)/2
-        y = pt1_y - (pt2_y * pt3)
+        pt1_y: float = (y_start + y_end)/2 if cw else (y_end + y_start)/2
+        pt2_y: float = (x_start - x_end)/2 if cw else (x_end - x_start)/2
+        y: float = pt1_y - (pt2_y * pt3)
 
         self.arc_to_lines(x_start, y_start, x, y, angle/2, cw)
         self.arc_to_lines(x, y, x_end, y_end, angle/2, cw)
