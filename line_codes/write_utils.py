757afb9f25f4589e61bc46cf97c2e6d92464a665
Hud Wahab <hudwahab@gmail.com>
2023-09-26 14:23:45 -0600

    LPRC Rev 3 -> LPRC, write_utilsRev2 -> write_utils

--- line_codes/write_utils.py
+++ line_codes/write_utils.py
@@ -5,9 +5,12 @@ from pathlib import Path
 from random import randrange
 from datetime import date
 
+
+
 def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
     power_range: set = (5,1190), time_range: set = (1050,5000), pressure_range: set = (100,350), \
-    batch: bool = False) -> None:
+     batch: bool = False, passes_range: set = np.arange(1,10),
+     defocus_range: set = (-3,0)) -> None:
         """ Writes data files with the parameter design at path:
             path: Path of the campaign
             series: Campaign number
@@ -19,6 +22,8 @@ def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
         power=[]
         line_time=[]
         pressure=[]
+        line_passes=[]
+        defocus = []
 
         print("TODAY's DATE:",str(date.today()))
 
@@ -26,10 +31,14 @@ def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
             powr = randrange(power_range[0], power_range[1], 1) # in mW
             tm = randrange(time_range[0], time_range[1], 1) # in ms
             pr = randrange(pressure_range[0], pressure_range[1], 10) # in psi
+            passes = randrange(passes_range[0], passes_range[1], 1) #passes
+            defoc = randrange(int(defocus_range[0]*100), int(defocus_range[1]*100),1)/100
             for i in range(9):
                 power.append(powr)
                 line_time.append(tm)
                 pressure.append(pr)
+                line_passes.append(passes)
+                defocus.append(defoc)
 
         print(f"POWER: {power_range}\n",
               f"TIME: {time_range}\n",
@@ -37,13 +46,13 @@ def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
 
         if batch:
             p = str(date.today()) + "-BATCH-" + str(series)
-            data_header=['power','time','pressure','ratio','resistance']
-            plot_header=['power','time','pressure','dummy_ratio', 'dummy_resistance', \
+            data_header=['power','time','pressure','passes','defocus','ratio','resistance']
+            plot_header=['power','time','pressure','passes','defocus','dummy_ratio', 'dummy_resistance', \
                          'pred_mean', 'pred_upper', 'ei']
         else:
             p = str(date.today()) + "-Series-" + str(series)
-            data_header=['power','time','pressure','ratio']
-            plot_header=['power','time','pressure','dummy_ratio', \
+            data_header=['power','time','pressure','passes','defocus','ratio']
+            plot_header=['power','time','pressure','passes','defocus','dummy_ratio', \
                         'pred_mean', 'pred_upper', 'ei']
 
         print(f"Here is the new Campaign Folder:{p}")
@@ -58,13 +67,13 @@ def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
         with open('dataset.csv', 'w', newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow(data_header)
-                writer.writerows(zip(power, line_time, pressure))
+                writer.writerows(zip(power, line_time, pressure,line_passes,defocus))
 
         #rename this to raw_pre-patterning.csv
         with open('dataset-pre.csv', 'w', newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow(data_header)
-                writer.writerows(zip(power, line_time, pressure))
+                writer.writerows(zip(power, line_time, pressure,line_passes,defocus))
 
         # THIS HAS THREE ADDITIONAL COLUMNS
         with open('plot_data.csv', 'w', newline='') as f:
@@ -107,12 +116,14 @@ def write_more() -> None:
     vpower = d['power'][ln-1]
     vtime = d['time'][ln-1]
     vpressure = d['pressure'][ln-1]
+    vpasses = d['passes'][ln-1]
 
     d1 = pd.read_csv('dataset.csv')
     ln = d1.shape[0]
     d1.loc[ln,"power"] = vpower
     d1.loc[ln,"time"] = vtime
     d1.loc[ln,"pressure"] = vpressure
+    d1.loc[ln,"passes"] = vpasses
     d1.to_csv('dataset.csv',index = False)
     d1.to_csv('dataset-pre.csv',index = False)
 def repeats() -> None:
@@ -123,7 +134,7 @@ def repeats() -> None:
     print(f"DATASET LENGTH: {ln}")
     counter  = m
     for i in range(8):
-        toAdd = [df2['power'][m-1],df2['time'][m-1],df2['pressure'][m-1]]
+        toAdd = [df2['power'][m-1],df2['time'][m-1],df2['pressure'][m-1],df2['passes'][m-1]]
         filename = "dataset.csv"
         with open(filename, "r") as infile:
             reader = list(csv.reader(infile))
@@ -135,7 +146,7 @@ def repeats() -> None:
                 writer.writerow(line)
                 
     for i in range(8):
-        toAdd = [df2['power'][m - 1],df2['time'][m - 1],df2['pressure'][m - 1]]
+        toAdd = [df2['power'][m - 1],df2['time'][m - 1],df2['pressure'][m - 1], df2['passes'][m - 1]]
         filename = "dataset-pre.csv"
         with open(filename, "r") as infile:
             reader = list(csv.reader(infile))
@@ -166,7 +177,23 @@ def get_mean(steps: int, save_line: int, spots_measured: int, target: str='ratio
     df2.to_csv('data.csv', index = False)
     return result
 
+# def take_mean(steps: int, save_line: int) -> float:
 
+#     print(os.getcwd())
+#     df = pd.read_csv('dataset.csv')
+
+#     valss = np.sort([df['ratio'][steps+8], df['ratio'][steps+7], df['ratio'][steps+6],
+#                     df['ratio'][steps+5], df['ratio'][steps+4], df['ratio'][steps+3],
+#                    df['ratio'][steps+2], df['ratio'][steps+1], df['ratio'][steps]])
+
+#     lst = [s for s in valss if str(s) != 'nan']
+#     result = np.mean(lst)
+#     df['ratio'].iloc[-len(valss):].fillna(0, inplace=True)
+#     df.to_csv('dataset.csv', index=False)
+#     df2 = pd.read_csv('data.csv')
+#     df2.loc[save_line,"ratio"] = result
+#     df2.to_csv('data.csv', index = False)
+#     return result
 
 def get_move_y(lines: int, start_y: int, step_y: int = 1) -> list:
     move_y = [0 for i in range(int(lines))]
