901d0a2ffca3b4ba784885f85f275f06f2de5f62
hududed <hudwahab@gmail.com>
2023-11-03 05:28:26 -0600

    more general example

--- line_codes/mlr3-batch-test.py
+++ line_codes/mlr3-batch-test.py
@@ -1,18 +1,26 @@
 # %%
 import os
-os.environ['R_HOME'] = "C:/Program Files/R/R-4.3.1"
+
+os.environ["R_HOME"] = "C:/Program Files/R/R-4.3.1"
 
 # %%
 import rpy2.robjects as robjects
 import os
 import pandas as pd
-from write_utils import create_new_path, write_data_files, duplicate_to_dataset
+from write_utils import (
+    create_new_path,
+    write_data_files,
+    write_laser_data_files,
+    duplicate_to_dataset,
+)
 
 from pathlib import Path
 
 # %%
-#Define the file you want to read
-path = Path(r'c:\\Users\\UWAdmin\\Desktop\\_pyControl\\campaigns')   #changes directory to new file created in next block. run this, run next block, update campaign_path, and run this again
+# Define the file you want to read
+path = Path(
+    r"c:\\Users\\UWAdmin\\Desktop\\_pyControl\\campaigns"
+)  # changes directory to new file created in next block. run this, run next block, update campaign_path, and run this again
 campaign_path = path / "mlr3-campaigns"
 if not os.path.exists(campaign_path):
     os.mkdir(campaign_path)
@@ -21,11 +29,20 @@ os.getcwd()
 
 
 # %%
-#Create new file (only need to run this when starting a new series of experiments)
+# Create new file (only need to run this when starting a new series of experiments)
 new_path = create_new_path(campaign_path, series=1, batch=True)
 os.chdir(new_path)
-write_data_files(new_path, series=1, nr_random_lines=6, \
-    power_range=(300,2600), time_range=(2000, 28000), pressure_range=(60,800),passes_range=(1,2),defocus_range=(-2.5,0))
+write_data_files(
+    campaign_path,
+    series=1,
+    nr_random_lines=4,
+    param1_range=(1, 20),
+    param2_range=(1, 10),
+    param3_range=(1, 100),
+)
+
+# write_laser_data_files(new_path, series=1, nr_random_lines=6, \
+#     power_range=(300,2600), time_range=(2000, 28000), pressure_range=(60,800),passes_range=(1,2),defocus_range=(-2.5,0))
 
 # set passes_range=(1,2) to set number of passes to 1 for all lines
 # values here need to be the same as values in red ML section
@@ -33,19 +50,15 @@ write_data_files(new_path, series=1, nr_random_lines=6, \
 # %%
 
 # Define the mlr3 R functions
-robjects.r('''
+robjects.r(
+    """
 library(mlr3mbo)
 library(mlr3)
 library(mlr3learners)
 library(bbotk)
 library(data.table)
 library(tibble)
-
-# convert real pressure to values applicable to parameter space e.g. 60 psi -> 1 in parameter space
-presToR <- function (x) {(x-50)/10}
-
-# convert parameter space pressure to real pressure e.g. 1 -> 60 psi
-presToExp <- function (x) {x*10+50}
+library(R.utils)
 
 load_archive <- function() {
   archive = readRDS("archive.rds")
@@ -61,6 +74,14 @@ save_archive <- function(archive, acq_function, acq_optimizer) {
   saveRDS(acq_optimizer, "acqopt.rds")
 }
 
+update_and_optimize <- function(acq_function, acq_optimizer, tmp_archive, candidate_new, lie) {
+  acq_function$surrogate$update()
+  acq_function$update()
+  tmp_archive$add_evals(xdt = candidate_new, xss_trafoed = transform_xdt_to_xss(candidate_new, tmp_archive$search_space), ydt = lie)
+  candidate_new = acq_optimizer$optimize()
+  return(candidate_new)
+}
+
 add_evals_to_archive <- function(archive, acq_function, acq_optimizer, data, q) {
   lie = data.table()
   liar = min
@@ -71,106 +92,110 @@ add_evals_to_archive <- function(archive, acq_function, acq_optimizer, data, q) 
   acq_function$surrogate$archive = tmp_archive
   lie[, archive$cols_y := liar(archive$data[[archive$cols_y]])]
   candidate_new = candidate
-  # loops through batch size q, e.g. q = 6 means batch of 6 lines
+  # loops through batch size q, e.g. q = 4 -> batch of 4
   for (i in seq_len(q)[-1L]) {
-    tmp_archive$add_evals(xdt = candidate_new, xss_trafoed = transform_xdt_to_xss(candidate_new, tmp_archive$search_space), ydt = lie)
-    acq_function$surrogate$update()
-    acq_function$update()
-    candidate_new = acq_optimizer$optimize()
+    candidate_new = update_and_optimize(acq_function, acq_optimizer, tmp_archive, candidate_new, lie)
     candidate = rbind(candidate, candidate_new)
   }
-  # convert pressure back to real values
-  candidate$pressure <- presToExp(candidate$pressure)
-  candidate$defocus <- format(round(candidate$defocus, 2), nsmall = 2)
-  save_archive(tmp_archive, acq_function, acq_optimizer)
-  return(list(candidate, tmp_archive, acq_function))
+  candidate_new = update_and_optimize(acq_function, acq_optimizer, tmp_archive, candidate_new, lie)
+  
+  save_archive(archive, acq_function, acq_optimizer)
+  return(list(candidate, archive, acq_function))
 }
 
 experiment <- function(s) {
-  if(s==1) {
+  if(s==1) { # create init model, and propose first candidates for evaluation
     require(XML)
     set.seed(42)
     data = subset(data.table(read.csv("data.csv")), colClasses = c(NA, NA, NA, NA, "NULL", NA))
-    data$pressure <- presToR(data$pressure)
-    search_space = ps(power = p_int(lower = 100, upper = 2600),
-                      time = p_int(lower = 2000, upper = 28000),
-                      pressure = p_int(lower = 1, upper = 75, trafo = function(x) x * 10 + 50),
-                      defocus = p_dbl(lower=-3.00, upper = 0.00))
-    codomain = ps(resistance = p_dbl(tags = "minimize"))
+    search_space = ps(param1 = p_int(lower = 1, upper = 10),
+                      param2 = p_int(lower = 1, upper = 10),
+                      param3 = p_int(lower = 1, upper = 100))
+    codomain = ps(output = p_dbl(tags = "maximize"))
     archive = Archive$new(search_space = search_space, codomain = codomain)
-    archive$add_evals(xdt = data[, c("power", "time", "pressure", "defocus")], ydt = data[, "resistance"])
+    archive$add_evals(xdt = data[, c("param1", "param2", "param3")], ydt = data[, "output"])
     print("Model archive so far: ")
     print(archive)
-    surrogate = srlrn(lrn("regr.km", control = list(trace = FALSE)), archive = archive)
+    surrogate = srlrn(lrn("regr.km", control = list(trace = FALSE, nugget.stability=1e-8)), archive = archive)
     acq_function = acqf("ei", surrogate = surrogate)
     acq_optimizer = acqo(opt("random_search", batch_size = 1000),
                          terminator = trm("evals", n_evals = 1000),
                          acq_function = acq_function)
-    q = 6
+    q = 4
     result = add_evals_to_archive(archive, acq_function, acq_optimizer, data, q)
-  } else {
+
+  } else { # update archive, propose further candidates for evaluation
     result = load_archive()
-    lines_to_keep <- 6 # should equal q
+    lines_to_keep <- 4 # should equal q
     num_lines <- countLines("data.csv")
     data <- as.data.table(read.csv("data.csv", header=FALSE, skip = num_lines-lines_to_keep))
-    names(data) <- c('power','time','pressure','passes','defocus', 'ratio','resistance')
-    data$pressure <- presToR(data$pressure)
+    names(data) <- c('param1','param2','param3','output')
+    print("New data: ")
+    print(data)
+           
     archive = result[[1]]
     acq_function = result[[2]]
     acq_optimizer = result[[3]]
-    archive$add_evals(xdt = data[, c("power", "time", "pressure", "defocus")], ydt = data[,"resistance"])
+    print(archive)
+
+    archive$add_evals(xdt = data[, c("param1", "param2", "param3")], ydt = data[,"output"])
     print("Model archive so far: ")
     print(archive)
-    q = 6
+    q = 4
     result = add_evals_to_archive(archive, acq_function, acq_optimizer, data, q)
   }
   candidate = result[[1]]
   archive = result[[2]]
   acq_function = result[[3]]
-  x2 <- candidate[,c("power","time","pressure", "defocus")]
-  x2 <- x2 %>% add_column(passes = 1, .after="pressure")
+  x2 <- candidate[,c("param1","param2","param3")]
+           
   print("New candidates: ")
   print(x2)
+  print("New archive: ")
+  print(archive)
+
   print("Writing to data.csv ...")
   write.table(x2, file = "data.csv", sep = ",",
               append = TRUE, quote = FALSE,col.names = FALSE, row.names = FALSE)
   print("WRITE DONE")
 }
-''')
+"""
+)
 
 # %%
-#Define running files
-import pandas as pd
 
-def _printing_line_count(data:pd.DataFrame, measured_value:str = 'resistance'):
-    printed_lines_count = len(data['resistance'].dropna(inplace=False))
+
+def _printing_line_count(data: pd.DataFrame, output: str):
+    printed_lines_count = len(data[output].dropna(inplace=False))
     return printed_lines_count
 
+
 data = pd.read_csv("data.csv")
-rsum = robjects.r['experiment']
+rsum = robjects.r["experiment"]
 
+OUTPUT = "output"
+actual_nr_lines = _printing_line_count(data, output=OUTPUT)
+_NR_PROPOSED_LINES = 4  # don't change this
 
-NR_LINES_IN_SAMPLE = _printing_line_count(data)
-_NR_PROPOSED_LINES = 6 # don't change this
-
-NR_LINES_IN_SAMPLE
+actual_nr_lines
 # %%
 
 # Don't run this section on first sample (iteration) of new campaign
 
-if len(data['resistance']) == NR_LINES_IN_SAMPLE:
+if actual_nr_lines == _NR_PROPOSED_LINES:
     # print(len(d['resistance']))
-    print(f"First batch proposal with {NR_LINES_IN_SAMPLE} lines to train")
+    print(f"First batch proposal with {_NR_PROPOSED_LINES} lines to train")
     rsum((1))
-    duplicate_to_dataset(nr_proposed_lines=_NR_PROPOSED_LINES, prepattern=False)
+    # duplicate_to_dataset(nr_proposed_lines=_NR_PROPOSED_LINES, prepattern=False)
 
-elif len(data['resistance']) < NR_LINES_IN_SAMPLE:
-    print ("Not enough data points in dataset. Exiting.")
-    exit()
+elif actual_nr_lines < _NR_PROPOSED_LINES:
+    print("Not enough data points in dataset. Please complete evaluation.")
+    # exit()
 
 else:
-    print(f"Further batch proposals of {NR_LINES_IN_SAMPLE} lines")
-    proposed = data['resistance'].iloc[-NR_LINES_IN_SAMPLE:]
-    rsum((list(proposed)))
-    duplicate_to_dataset(nr_proposed_lines=_NR_PROPOSED_LINES)
+    print(f"Further batch proposals of {_NR_PROPOSED_LINES} lines")
+    proposed = data[OUTPUT].iloc[-_NR_PROPOSED_LINES:]
+    # print(proposed)
+    rsum((2))
+    # duplicate_to_dataset(nr_proposed_lines=_NR_PROPOSED_LINES)
 # %%
--- line_codes/write_utils.py
+++ line_codes/write_utils.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
-import os,csv
+import os, csv
 from pathlib import Path
 from random import randrange
 from datetime import date
 
 
 def create_new_path(path: Path, series: int, batch: bool = False) -> Path:
-    """ Creates a new path for the campaign
-        path: Path of the campaign
-        series: Campaign number
+    """Creates a new path for the campaign
+    path: Path of the campaign
+    series: Campaign number
     """
     if batch:
         p = str(date.today()) + "-BATCH-" + str(series)
@@ -25,32 +25,78 @@ def create_new_path(path: Path, series: int, batch: bool = False) -> Path:
     return Path(os.getcwd())
 
 
-def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
-    power_range: set = (5,1190), time_range: set = (1050,5000), pressure_range: set = (100,350), \
-     batch: bool = False, passes_range: set = np.arange(1,10),
-     defocus_range: set = (-3,0)) -> Path:
-    """ Writes data files with the parameter design at path:
-        path: Path of the campaign
-        series: Campaign number
-        nr_random_lines: Number of starting lines, defaults at 7
-        power_range: set of minPower, maxPower, default (5, 1190)
-        time_range: set of minTime, maxTime, default (1050, 5000)
-        pressure_range: set of minPressure, maxPressure, default (100,350)
-    """
-    power=[]
-    line_time=[]
-    pressure=[]
-    line_passes=[]
-    defocus = []
+def write_data_files(
+    path: Path,
+    series: int,
+    nr_random_lines: int = 7,
+    param1_range: tuple = (1, 10),
+    param2_range: tuple = (1, 10),
+    param3_range: tuple = (1, 100),
+    batch: bool = True,
+) -> Path:
+    param1 = []
+    param2 = []
+    param3 = []
+    output = []
 
-    print("TODAY's DATE:",str(date.today()))
+    print("TODAY's DATE:", str(date.today()))
 
     for x in range(nr_random_lines):
-        powr = randrange(power_range[0], power_range[1], 1) # in mW
-        tm = randrange(time_range[0], time_range[1], 1) # in ms
-        pr = randrange(pressure_range[0], pressure_range[1], 10) # in psi
-        passes = randrange(passes_range[0], passes_range[1], 1) #passes
-        defoc = randrange(int(defocus_range[0]*100), int(defocus_range[1]*100),1)/100
+        param1_value = randrange(param1_range[0], param1_range[1], 1)
+        param2_value = randrange(param2_range[0], param2_range[1], 1)
+        param3_value = randrange(param3_range[0], param3_range[1], 1)
+
+        param1.append(param1_value)
+        param2.append(param2_value)
+        param3.append(param3_value)
+        output.append(None)
+
+    # Create a DataFrame with 'param1', 'param2', 'param3', and 'output' as columns
+    df = pd.DataFrame(
+        {"param1": param1, "param2": param2, "param3": param3, "output": output}
+    )
+
+    # Write the DataFrame to 'data.csv'
+    df.to_csv("data.csv", index=False)
+
+    return path
+
+
+def write_laser_data_files(
+    path: Path,
+    series: int,
+    nr_random_lines: int = 7,
+    power_range: set = (5, 1190),
+    time_range: set = (1050, 5000),
+    pressure_range: set = (100, 350),
+    batch: bool = False,
+    passes_range: set = np.arange(1, 10),
+    defocus_range: set = (-3, 0),
+) -> Path:
+    """Writes data files with the parameter design at path:
+    path: Path of the campaign
+    series: Campaign number
+    nr_random_lines: Number of starting lines, defaults at 7
+    power_range: set of minPower, maxPower, default (5, 1190)
+    time_range: set of minTime, maxTime, default (1050, 5000)
+    pressure_range: set of minPressure, maxPressure, default (100,350)
+    """
+    power = []
+    line_time = []
+    pressure = []
+    line_passes = []
+    defocus = []
+
+    print("TODAY's DATE:", str(date.today()))
+
+    for x in range(nr_random_lines):
+        powr = randrange(power_range[0], power_range[1], 1)  # in mW
+        tm = randrange(time_range[0], time_range[1], 1)  # in ms
+        pr = randrange(pressure_range[0], pressure_range[1], 10)  # in psi
+        passes = randrange(passes_range[0], passes_range[1], 1)  # passes
+        defoc = (
+            randrange(int(defocus_range[0] * 100), int(defocus_range[1] * 100), 1) / 100
+        )
         for i in range(9):
             power.append(powr)
             line_time.append(tm)
@@ -58,41 +104,81 @@ def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
             line_passes.append(passes)
             defocus.append(defoc)
 
-    print(f"POWER: {power_range}\n",
-          f"TIME: {time_range}\n",
-          f"PRESSURE: {pressure_range}")
+    print(
+        f"POWER: {power_range}\n",
+        f"TIME: {time_range}\n",
+        f"PRESSURE: {pressure_range}",
+    )
 
-    data_header=['power','time','pressure','passes','defocus','ratio', 'resistance']
-    plot_header=['power','time','pressure','passes','defocus','dummy_ratio', \
-                'pred_mean', 'pred_upper', 'ei']
+    data_header = [
+        "power",
+        "time",
+        "pressure",
+        "passes",
+        "defocus",
+        "ratio",
+        "resistance",
+    ]
+    plot_header = [
+        "power",
+        "time",
+        "pressure",
+        "passes",
+        "defocus",
+        "dummy_ratio",
+        "pred_mean",
+        "pred_upper",
+        "ei",
+    ]
 
-    #rename this to raw_post_processed.csv
-    with open('dataset.csv', 'w', newline='') as f:
+    # rename this to raw_post_processed.csv
+    with open("dataset.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(data_header)
-        writer.writerows(zip(power, line_time, pressure,line_passes,defocus))
+        writer.writerows(zip(power, line_time, pressure, line_passes, defocus))
 
-    #rename this to raw_pre-patterning.csv
-    with open('dataset-pre.csv', 'w', newline='') as f:
+    # rename this to raw_pre-patterning.csv
+    with open("dataset-pre.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(data_header)
-        writer.writerows(zip(power, line_time, pressure,line_passes,defocus))
+        writer.writerows(zip(power, line_time, pressure, line_passes, defocus))
 
     # THIS HAS THREE ADDITIONAL COLUMNS
-    with open('plot_data.csv', 'w', newline='') as f:
+    with open("plot_data.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(plot_header)
 
     # THIS HAS THREE ADDITIONAL COLUMNS
-    with open('fit_results.csv', 'w', newline='') as f:
-        fit_header=['D','PD','WD','FD','D1','PD1','WD1','FD1',\
-                    'G','PG','WG','FG','2D','P2D','W2D','F2D','GD','2DG','file']
+    with open("fit_results.csv", "w", newline="") as f:
+        fit_header = [
+            "D",
+            "PD",
+            "WD",
+            "FD",
+            "D1",
+            "PD1",
+            "WD1",
+            "FD1",
+            "G",
+            "PG",
+            "WG",
+            "FG",
+            "2D",
+            "P2D",
+            "W2D",
+            "F2D",
+            "GD",
+            "2DG",
+            "file",
+        ]
         writer = csv.writer(f)
         writer.writerow(fit_header)
 
-    df2=pd.read_csv('dataset.csv')
-    df2=df2.drop_duplicates() #keep only the unique rows
-    df2.to_csv('data.csv',index=False) #this is what will be read by mlrMBO in th R code
+    df2 = pd.read_csv("dataset.csv")
+    df2 = df2.drop_duplicates()  # keep only the unique rows
+    df2.to_csv(
+        "data.csv", index=False
+    )  # this is what will be read by mlrMBO in th R code
 
     return Path(os.getcwd())
 
@@ -182,96 +268,104 @@ def write_data_files(path: Path, series: int, nr_random_lines: int = 7, \
 #         df2.head()
 #         df2.to_csv('data.csv',index=False) #this is what will be read by mlrMBO in th R code
 
-def duplicate_to_dataset(nr_proposed_lines: int = 1, prepattern: bool = False, rep: int = 9) -> None:
-    data = pd.read_csv('data.csv')
-    dataset = pd.read_csv('dataset.csv')
+
+def duplicate_to_dataset(
+    nr_proposed_lines: int = 1, prepattern: bool = False, rep: int = 9
+) -> None:
+    data = pd.read_csv("data.csv")
+    dataset = pd.read_csv("dataset.csv")
 
     proposed = data.iloc[-nr_proposed_lines:]
-    repeated_data = pd.DataFrame(np.repeat(proposed.values, rep, axis = 0))
+    repeated_data = pd.DataFrame(np.repeat(proposed.values, rep, axis=0))
     repeated_data.columns = proposed.columns
     new_dataset = pd.concat([dataset, repeated_data])
-    new_dataset.to_csv('dataset.csv',index = False)
+    new_dataset.to_csv("dataset.csv", index=False)
     if prepattern:
-        dataset_pre = pd.read_csv('dataset-pre.csv')
+        dataset_pre = pd.read_csv("dataset-pre.csv")
         new_dataset_pre = pd.concat([dataset_pre, repeated_data])
-        new_dataset_pre.to_csv('dataset-pre.csv',index = False)
-    
+        new_dataset_pre.to_csv("dataset-pre.csv", index=False)
+
+
 def write_more() -> None:
     """
     Used after doing the AI stuff
     """
-    d = pd.read_csv('data.csv')
+    d = pd.read_csv("data.csv")
     ln = d.shape[0]
 
-    vpower = d['power'][ln-1]
-    vtime = d['time'][ln-1]
-    vpressure = d['pressure'][ln-1]
+    vpower = d["power"][ln - 1]
+    vtime = d["time"][ln - 1]
+    vpressure = d["pressure"][ln - 1]
 
-    d1 = pd.read_csv('dataset.csv')
+    d1 = pd.read_csv("dataset.csv")
     ln = d1.shape[0]
-    d1.loc[ln,"power"] = vpower
-    d1.loc[ln,"time"] = vtime
-    d1.loc[ln,"pressure"] = vpressure
-    d1.to_csv('dataset.csv',index = False)
-    d1.to_csv('dataset-pre.csv',index = False)
+    d1.loc[ln, "power"] = vpower
+    d1.loc[ln, "time"] = vtime
+    d1.loc[ln, "pressure"] = vpressure
+    d1.to_csv("dataset.csv", index=False)
+    d1.to_csv("dataset-pre.csv", index=False)
+
+
 def repeats() -> None:
     # d1 = pd.read_csv('data.csv')
-    df2 = pd.read_csv('dataset.csv')
-    ln = len(df2['power'])
+    df2 = pd.read_csv("dataset.csv")
+    ln = len(df2["power"])
     m = ln
     print(f"DATASET LENGTH: {ln}")
-    counter  = m
+    counter = m
     for i in range(8):
-        toAdd = [df2['power'][m-1],df2['time'][m-1],df2['pressure'][m-1]]
+        toAdd = [df2["power"][m - 1], df2["time"][m - 1], df2["pressure"][m - 1]]
         filename = "dataset.csv"
         with open(filename, "r") as infile:
             reader = list(csv.reader(infile))
-            reader.insert(counter+1, toAdd)
+            reader.insert(counter + 1, toAdd)
 
-        with open(filename, "w", newline='') as outfile:
+        with open(filename, "w", newline="") as outfile:
             writer = csv.writer(outfile)
             for line in reader:
                 writer.writerow(line)
-                
+
     for i in range(8):
-        toAdd = [df2['power'][m - 1],df2['time'][m - 1],df2['pressure'][m - 1]]
+        toAdd = [df2["power"][m - 1], df2["time"][m - 1], df2["pressure"][m - 1]]
         filename = "dataset-pre.csv"
         with open(filename, "r") as infile:
             reader = list(csv.reader(infile))
             reader.insert(counter + 1, toAdd)
 
-        with open(filename, "w", newline='') as outfile:
+        with open(filename, "w", newline="") as outfile:
             writer = csv.writer(outfile)
             for line in reader:
                 writer.writerow(line)
 
-def get_mean(steps: int, save_line: int, spots_measured: int, target: str='ratio') -> float:
 
-    """ Extracts the mean of the last 'steps' lines of the dataset.csv file
-        to data.csv
+def get_mean(
+    steps: int, save_line: int, spots_measured: int, target: str = "ratio"
+) -> float:
+    """Extracts the mean of the last 'steps' lines of the dataset.csv file
+    to data.csv
     """
 
     print(os.getcwd())
-    df = pd.read_csv('dataset.csv')
+    df = pd.read_csv("dataset.csv")
     # spots_measured = save_line/steps
-    result = df[target].iloc[steps:steps + spots_measured].mean()
+    result = df[target].iloc[steps : steps + spots_measured].mean()
     print(f"Filling nans with 0")
-    df[target][steps:steps + spots_measured].fillna(0, inplace=True)
-    df.to_csv('dataset.csv', index=False)
+    df[target][steps : steps + spots_measured].fillna(0, inplace=True)
+    df.to_csv("dataset.csv", index=False)
 
-    df2 = pd.read_csv('data.csv')
-    print(f'saving mean {result} to data.csv')
+    df2 = pd.read_csv("data.csv")
+    print(f"saving mean {result} to data.csv")
     df2.loc[save_line, target] = result
-    df2.to_csv('data.csv', index = False)
+    df2.to_csv("data.csv", index=False)
     return result
-
 
 
 def get_move_y(lines: int, start_y: int, step_y: int = 1) -> list:
     move_y = [0 for i in range(int(lines))]
     for i in range(int(lines)):
-    #     print(i)
-        if i == 0: move_y[i] = float(start_y)
-        if i > 0 :
-            move_y[i] = float(move_y[i-1]) + float(step_y)
+        #     print(i)
+        if i == 0:
+            move_y[i] = float(start_y)
+        if i > 0:
+            move_y[i] = float(move_y[i - 1]) + float(step_y)
     return move_y
